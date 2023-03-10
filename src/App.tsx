7fce34a1975dc63a92efe18fe966736b8ce903f6
yuraba <yurikulikov22@gmail.com>
2023-03-10 17:48:25 +0200

    inital

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let [title,setTitle] = useState('Свіфт говно')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button className='button' onClick={()=>{
+              let number = Math.floor(Math.random() * 101);
+              if (number > 50){
+                  setTitle('Так, він йде нахуй')
+              }else {
+                  setTitle('Не сьогодні,  козаче')
+              }
+          }}>Чи вигнали Володю?</button>
+          <h1>{title}</h1>
       </header>
     </div>
   );
